fabf74bcb3239d83f2a7fdccc6a156dbebb72d77
Ling Yen Lee <lingyenlee@avertro.com>
2020-11-22 23:06:44 +1100

    connect disconnect user

--- index.js
+++ index.js
@@ -11,15 +11,22 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/public/index.html');
 });
 
+// app.get('/', (req, res) => {
+//   res.sendFile(__dirname + '/public/index.html');
+// });
+
 io.on('connection', (socket) => {
   console.log('user connected');
 
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
   //   socket.emit('message', { lee: 'hey how are you?' });
   //   socket.on('another event', (data) => {
   //     console.log(data);
   //   });
-  socket.emit('greetings', 'Hey!', { ms: 'jane' }, Buffer.from([4, 3, 3, 1]));
-  socket.on('salutations', (elem1, elem2, elem3) => {
-    console.log(elem1, elem2, elem3);
-  });
+  //   socket.emit('greetings', 'Hey!', { ms: 'jane' }, Buffer.from([4, 3, 3, 1]));
+  //   socket.on('salutations', (elem1, elem2, elem3) => {
+  //     console.log(elem1, elem2, elem3);
+  //   });
 });
